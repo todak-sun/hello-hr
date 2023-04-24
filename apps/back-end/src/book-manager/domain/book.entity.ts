3106eaacd8430f15c6a214906244370aa59fe337
todak-sun <tjsdydwn@gmail.com>
2023-04-24 17:49:34 +0900

    feat: 도서 & 강의 관련 기능 구현 시작

---
+++ apps/back-end/src/book-manager/domain/book.entity.ts
@@ -0,0 +1,23 @@
+import { LocalDateTime } from "@js-joda/core";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { LocalDateTimeTransformer } from "../../common/local-date-time.transformer";
+
+@Entity({
+  name: "book",
+})
+export class BookEntity {
+  @PrimaryGeneratedColumn({ name: "book_id" })
+  id: number;
+
+  @Column({ name: "name" })
+  name: string;
+
+  @Column({ name: "ISBN" })
+  isbn: string;
+
+  @Column({ name: "updated_datetime", transformer: new LocalDateTimeTransformer() })
+  updatedDateTime: LocalDateTime;
+
+  @Column({ name: "created_datetime", transformer: new LocalDateTimeTransformer() })
+  createdDateTime: LocalDateTime;
+}
